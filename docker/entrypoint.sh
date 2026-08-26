e857f64da2f54e3bc85c767dc345fa1344f498b2
Paulo Correia <pcportugal@gmail.com>
2026-08-26 01:29:15 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,57 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-# Allows Ctrl-C, by letting this sh process act as PID 1
-exit_func() {
-    exit 1
-}
-trap exit_func TERM INT
+CONFIG_PATH=/data/options.json
 
-ulimit -n 65535
+MODE=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
+LOG=$(jq --raw-output '.log // empty' $CONFIG_PATH)
 
-echo "Running: ${*}"
-"$@"
+if [ -f "/homeassistant/addons/neolink.toml" ]; then
+    echo "Migrating '/homeassistant/addons/neolink.toml' to '/addon_configs/a14d3924_neolink-latest/neolink.toml'"
+    cp /homeassistant/addons/neolink.toml /config/
+    mv /homeassistant/addons/neolink.toml /homeassistant/addons/neolink.toml.migrated
+fi
+
+echo "--- VERSIONS ---"
+echo "App version: 0.1.1"
+echo -n "neolink version: " && neolink --version
+echo "neolink mode: ${MODE}"
+echo "neolink log: ${LOG}"
+echo "ATTENTION: if you expected a newer Neolink version, please reinstall this App!"
+echo "--- Neolink ---"
+
+case $LOG in
+  debug)
+    export RUST_LOG="neolink=debug"
+    ;;
+  info)
+    export RUST_LOG="neolink=info"
+    ;;
+  warn)
+    export RUST_LOG="neolink=warn"
+    ;;
+  error)
+    export RUST_LOG="neolink=error"
+    ;;
+  *)
+    echo -n "Unknown log level"
+    ;;
+esac
+
+case $MODE in
+  rtsp)
+    neolink rtsp --config /config/neolink.toml
+    ;;
+
+  mqtt)
+    neolink mqtt --config /config/neolink.toml
+    ;;
+
+  dual)
+    neolink mqtt-rtsp --config /config/neolink.toml
+    ;;
+
+  *)
+    echo -n "Unknown mode option"
+    ;;
+esac
